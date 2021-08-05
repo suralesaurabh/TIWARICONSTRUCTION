95402e629187c2a7da10c6976b0d85dc2e22eb11
suralesaurabh <suralesaurabh333@gmail.com>
2021-08-05 23:02:17 +0530

    added files

---
+++ civilSite/navigation.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+   <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+	<title>Saurabh Surale</title>
+<!-- <link rel="shortcut icon" href="img/favicon.ico">  -->
+	<!-- Bootstrap 4.5 CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Style CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Google Fonts -->
+	
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/fd6460c960.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+	<!-- Top Bar -->
+    <div class="top-bar">
+		<div class="container">
+			<div class="col-12 text-right">
+				<p><a href="tel:+917588272173">Call us +91 7588272173 
+				</a></p>
+			</div>
+		</div>
+	</div>
+	<!-- End Top Bar -->
+
+
+	<!-- Navigation -->
+     <nav class="navbar bg-light navbar-light navbar-expand-lg">
+		 <div class="container">
+
+          <a href="index.html" class="navbar-brand">
+			  <img src="img/logo.jpg" class="img-fluid"
+			alt="Logo" title="Logo" /></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+		data-target="#navbarResponsive">
+	    <Span class="navbar-toggler-icon"></Span>
+    </button>
+
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
+			<li class="nav-item"><a href="./about/index.php" class="nav-link">About</a></li>
+			<li class="nav-item"><a href="" class="nav-link">Services</a></li>
+			<li class="nav-item"><a href="" class="nav-link">Projects</a></li>
+			<li class="nav-item"><a href="" class="nav-link">Gallery</a></li>
+			<li class="nav-item"><a href="" class="nav-link">Contact Us</a></li>
+			<li><button type="button" class="btn btn-primary" style="color:white;margin-left: 10px; margin-top: 7px;">Sign Up</button>
+</li>
+		</ul>
+
+	</div>
+
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+
+	</div>
+
+		 </div>
+	 </nav>
